6838bfbc8da72894c456775b6f1ee363af98982b
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-06-25 09:34:49 -0300

    modific

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,8 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
+
+    <div class="nav-item dropdown" style="width:160px;height:40px">      
+         <a  class="btn btn-info active btn-lg"  id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            
+             <img src="{{asset('img/images (3).jpg')}}" width="100" /><br>
+             <span class="label label-default">Administrador </span> 
+          </a>
+
+
+          
+          <ul class="dropdown-menu multi-level"  style="width:270px"  role="menu" aria-labelledby="navbarDropdowndropdownMenu"> 
+
+        
+            <div class="form-login text-center">
+            <h4>Welcome back.</h4>
+            
+            
+           
+            </div>
+        
+        
+   
+                <li><div class="input-group dropdown-item">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input id="email" type="email" style="width:200px;height:40px" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }} " name="email" value="{{ old('email') }}" required autofocus  placeholder="Correo electrónico">
+                        @if ($errors->has('email'))
+                        <span class="invalid-feedback">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div></li> 
+
+                <li class="divider"><br></li>
+
+                <li><div class="input-group dropdown-item">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <input id="password" type="password" style="width:200px;height:40px"  class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required placeholder="Contraseña"> 
+                        @if ($errors->has('password'))
+                        <span class="invalid-feedback">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                        @endif
+                </div></li>
+                     
+                <li class="divider"><br></li>
+                <div class="dropdown-item">
+                   <li><button type="submit" class="btn btn-default ">Aceptar</button></li> 
+               </div>
+          </ul>
+       </div>
 
     	<a class="btn btn-info active btn-lg" role="button" href="{{ route('login') }}">
              <img src="{{asset('img/images (3).jpg')}}" width="100" /><br>
@@ -17,6 +68,7 @@
               </div>
           </div> 
         </div>
+
       
                  {{-- Panel de botones --}}
                     <div class="row">
