20d0d9500dc68ae8065a69b6e944cec45a4536ff
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-08-27 17:08:02 -0300

    registro responsable

--- resources/views/responsable/registro.blade.php
+++ resources/views/responsable/registro.blade.php
@@ -5,7 +5,7 @@
 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header text-center"><h4>{{ __('Registro Responsable') }}</h4> 
                      <div class="pull-right"><a href="#biobanco"  class="btn  btn-info  right" type='button'  data-toggle="modal" >Biobanco <i class="glyphicon-plus" ></i></a></div>
@@ -79,9 +79,9 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Usuario') }}</label>
+                            <label for="name" class="col-md-2 col-form-label ">{{ __('Usuario') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
@@ -90,14 +90,10 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
 
-                        <input type="hidden" value="responsable" name="rol">
+                            <label for="email" class="col-md-2 col-form-label ">{{ __(' Correo Electrónico') }}</label>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __(' Correo Electrónico') }}</label>
-
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
@@ -108,12 +104,44 @@
                             </div>
                         </div>
 
-                      
+                        <input type="hidden" value="responsable" name="rol">
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
+                            <label  class="col-md-2 col-form-label -">{{ __('Nombre') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-4">
+                                <input id="nombre" type="text" class="form-control" name="nombre" value="{{ old('name') }}" required autofocus>
+                            </div>
+                            <label  class="col-md-2 col-form-label ">{{ __('Apellido') }}</label>
+                             <div class="col-md-4">
+                                <input id="apellido" type="text" class="form-control" name="apellido" value="{{ old('name') }}" required autofocus>                            
+                            </div>
+                        </div>
+
+                         <div class="form-group row">
+                            <label  class="col-md-2 col-form-label -">{{ __('Teléfono de Contacto') }}</label>
+
+                            <div class="col-md-4">
+                                <input id="nombre" type="text" class="form-control" name="telefono" value="{{ old('name') }}" required autofocus>
+                            </div>
+                            <label  class="col-md-2 col-form-label ">{{ __('Hospital') }}</label>
+                             <div class="col-md-4">
+                                <input id="apellido" type="text" class="form-control" name="hospital" value="{{ old('name') }}" required autofocus>                            
+                            </div>
+                        </div>
+                         <div class="form-group row">
+                            <label  class="col-md-2 col-form-label -">{{ __('Lugar de Trabajo') }}</label>
+
+                            <div class="col-md-10">
+                                <input id="nombre" type="text" class="form-control" name="trabajo" value="{{ old('name') }}" required autofocus>
+                            </div>
+                            
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-2 col-form-label ">{{ __('Contraseña') }}</label>
+
+                            <div class="col-md-4">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -122,12 +150,12 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
+                        
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Contraseña') }}</label>
+                       
+                            <label for="password-confirm" class="col-md-2 col-form-label ">{{ __('Confirmar Contraseña') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
