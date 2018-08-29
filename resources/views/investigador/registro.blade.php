8ef97d5ad574c6d2803d9f3e488cef57d1ec6c02
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-08-29 16:16:56 -0300

    registro técnico y investigador
    vistas

--- resources/views/investigador/registro.blade.php
+++ resources/views/investigador/registro.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header text-center"><h4>{{ __('Registro Investigador') }}</h4>
                     <div class="pull-right"><a href="#seleccion"  class="btn  btn-info  right" type='button'  data-toggle="modal" >Seleccionar <i class="glyphicon-plus" ></i></a></div>
@@ -28,16 +28,16 @@
                                           <table class="table table-user-information">
                                             <tbody>
                                               <tr>
-                                                <td>Representante:</td>
+                                                <td>Biobanco:</td>
                                                
                                                 <td><select   >
-                                                    <option value="">Representante</option>
+                                                    <option value="">Biobanco</option>
                                                      
                                                 </select> </td>
                                                
                                               </tr>
                                              
-                                                <td><div class="panel-title ">Datos de Biobanco</div> </td>
+                                                <td><div class="panel-title ">Datos de Representante</div> </td>
                                                 
                                              
                                               <tr>
@@ -46,11 +46,11 @@
                                               </tr>
                                               
                                               <tr>
-                                                <td>Descrición</td>
+                                                <td>Apellido</td>
                                                  <td><label for=""></label></td>
                                               </tr>
                                               <tr>
-                                                <td>Telefono:</td>
+                                                <td>Telefono de contacto:</td>
                                                  <td><label for=""></label></td>
                                               </tr>
                                             </tbody>
@@ -82,11 +82,21 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-                        
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Usuario') }}</label>
+                        <div class="col-md-4 col-lg-3 " align="center">
+                           <div class="col-lg-offset-2 " > 
+                            <img alt="User Pic" src="{{asset('img/default.jpg')}}"  style="width:120px; height:120px; float:left; margin-right:50px;" id="profile-image1" class=" img-responsive"> 
+                            </div>
+                                 
+                                  <input id="profile-image-upload" value="Seleccionar imagen" name="avatar" class="hidden" type="file" accept="image/*" >
+                                      <div   style="color:#999;" >haga clic aquí para cambiar la imagen del perfil</div>  
+                                       <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                          {{-- <input type="submit" value="Cargar" class="pull btn btn-sm btn-default active">  --}}           
+                        </div>
 
-                            <div class="col-md-6">
+                        <div class="form-group row">
+                            <label for="name" class="col-md-2 col-form-label ">{{ __('Usuario') }}</label>
+
+                            <div class="col-md-4">
                                 <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
@@ -95,14 +105,10 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
 
-                        <input type="hidden" value="investigador" name="rol">
-                         
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __(' Correo Electrónico') }}</label>
+                            <label for="email" class="col-md-2 col-form-label ">{{ __(' Correo Electrónico') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
@@ -113,11 +119,44 @@
                             </div>
                         </div>
 
-                   
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
+                        <input type="hidden" value="responsable" name="rol">
 
-                            <div class="col-md-6">
+                        <div class="form-group row">
+                            <label  class="col-md-2 col-form-label -">{{ __('Nombre') }}</label>
+
+                            <div class="col-md-4">
+                                <input id="nombre" type="text" class="form-control" name="nombre" value="{{ old('name') }}" required autofocus>
+                            </div>
+                            <label  class="col-md-2 col-form-label ">{{ __('Apellido') }}</label>
+                             <div class="col-md-4">
+                                <input id="apellido" type="text" class="form-control" name="apellido" value="{{ old('name') }}" required autofocus>                            
+                            </div>
+                        </div>
+
+                         <div class="form-group row">
+                            <label  class="col-md-2 col-form-label -">{{ __('Teléfono de Contacto') }}</label>
+
+                            <div class="col-md-4">
+                                <input id="nombre" type="text" class="form-control" name="telefono" value="{{ old('name') }}" required autofocus>
+                            </div>
+                            <label  class="col-md-2 col-form-label ">{{ __('Hospital') }}</label>
+                             <div class="col-md-4">
+                                <input id="apellido" type="text" class="form-control" name="hospital" value="{{ old('name') }}" required autofocus>                            
+                            </div>
+                        </div>
+                         <div class="form-group row">
+                            <label  class="col-md-2 col-form-label -">{{ __('Lugar de Trabajo') }}</label>
+
+                            <div class="col-md-10">
+                                <input id="nombre" type="text" class="form-control" name="trabajo" value="{{ old('name') }}" required autofocus>
+                            </div>
+                            
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-2 col-form-label ">{{ __('Contraseña') }}</label>
+
+                            <div class="col-md-4">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -126,15 +165,19 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
+                        
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Contraseña') }}</label>
+                       
+                            <label for="password-confirm" class="col-md-2 col-form-label ">{{ __('Confirmar Contraseña') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
+                         <div class="form-group row">
+                              <label for="password" class="col-md-6 col-form-label ">Adjunte archivo PDF de certificacion por GRABIO </label>
+                              <input type="file" accept="application/pdf" /> 
+                         </div>
 
                        {{--  <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
@@ -151,6 +194,7 @@
                             <a href="{{ url('/') }}" data-original-title="cancelar" data-toggle="tooltip" role ="button" type="button" class="btn  btn-danger  ">Cancelar<i class="glyphicon glyphicon-share-alt"></i></a>
                             </span>
                     </div>
+                       
                     </form>
                 </div>
             </div>
