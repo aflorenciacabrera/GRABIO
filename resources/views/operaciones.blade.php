18f315b349d5df3b9dd533cd47715050c98585ab
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-10-11 16:31:29 -0300

    viales

--- resources/views/operaciones.blade.php
+++ resources/views/operaciones.blade.php
@@ -3,106 +3,17 @@
 @section('content')
 <div class="col-md-10 col-md-offset-0" class="text-center" > 
 	<div class="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12 center">
-            <h1 class="gallery-title text-center">Operaciones</h1> 
+            <h1 class="gallery-title text-center page-header">Operaciones</h1> 
      </div>
         
-		 {{-- <form method="POST" action="{{url('#')}}">
-						@csrf
-		 --}}
 
-		<button class="pull-right" type="submit" class="btn btn-success">{{ __('Guardar') }} </button>
-		<h3 class="page-header">Viales</h3>
-		<div align="center">
-		
-				 <select name="" id="">
-					 <option value="">seleccionar muestra</option>
-					 @foreach ($muestras as $muestra)
-					<option value="{{$muestra->id}}">{{$muestra->muestra}}</option>
-					@endforeach
-				</select>
-				{{-- <th><label for="">Muestras:</label>  </th> --}}
-				 <button  type="submit" class="btn btn-default ">{{ __('Ver detalles') }} </button>
-				<br></br>
- 		</div>
-
-        <div class="gallery center col-md-18 col-md-offset-0">
-    <div class="row">
-		
-    	    <h1 class="page-header"></h1>
-            <table class="table table-stripped table-bordered">
-			      <thead>
-			        <tr>
-			          <th>#</th>
-			          <th>Naturaleza</th>
-			          <th>Cantidad</th>
-			          <th>Concentración</th>
-			          <th>Fecha de Entrada</th>
-			          <th>Fecha de Salida</th>
-			          <th>Órgano</th>
-			          <th>Diagnóstico</th>
-			          <th>Ceder a Receptor</th>
-			          
-			        </tr>
-			      </thead>
-			      <tbody>
-					
-			        <tr>
-			          <th scope="row">V <label for="">1</label></th>
-			          	  <td><input type="text" name="naturaleza" class="form-control"></td>
-				          <td><input type="text" name="cantidad" class="form-control"></td>
-				          <td><input type="text" name="concentracion" class="form-control"></td>
-				          <td><input type="text" name="fecha_entrada" class="form-control"></td>
-				          <td><input type="text" name="fecha_salida" class="form-control"></td>
-				          <td><input type="text" name="organo" class="form-control"></td>
-				          <td><input type="text" name="diagnostico" class="form-control"></td>
-				         <td><button class="editbtn">Ceder</button></td>
-				         
-			        </tr>
-
-			        <tr>
-			          <th scope="row">V 2</th>
-			          	  <td><input type="text" name="" class="form-control"></td>
-				          <td><input type="text" name="" class="form-control"></td>
-				          <td><input type="text" name="" class="form-control"></td>
-				          <td><input type="text" name="" class="form-control"></td>
-				          <td><input type="text" name="" class="form-control"></td>
-				          <td><input type="text" name="" class="form-control"></td>
-				          <td><input type="text" name="" class="form-control"></td>
-				          <td><button class="editbtn">Ceder</button></td>
-				          
-			        </tr>
-			        <tr>
-			          <th scope="row">V 3</th>
-			          	  <td><input type="text" name="" class="form-control"></td>
-				          <td><input type="text" name="" class="form-control"></td>
-				          <td><input type="text" name="" class="form-control"></td>
-				          <td><input type="text" name="" class="form-control"></td>
-				          <td><input type="text" name="" class="form-control"></td>
-				          <td><input type="text" name="" class="form-control"></td>
-				          <td><input type="text" name="" class="form-control"></td>
-				         <td><button class="editbtn">Ceder</button></td>
-				        
-			        </tr>
-			      </tbody>
-    		</table>
-			
 	
-	</div>
-</div></form>
-		 {{-- <div align="center">
-		        	
-				 <button  type="button" class="btn btn-primary" data-toggle="modal" data-target="">
-				    Guardar</button> 
-		</div> --}}
+		
 
+    
+		 
 </div>
-{{-- <script>	
-$(document).ready(function(){
-    $('.editbtn').click(function(){
-        $(this).html($(this).html() == 'Guardar' ? 'Ceder' : 'Guardar');
-    });
-});
-	   </script> --}}
+
 	   
 	   
            
--- resources/views/viales/corteMuestra.blade.php
+++ resources/views/viales/corteMuestra.blade.php
@@ -18,20 +18,66 @@
 				@endforeach
 				</select>
 				{{-- <div class="pull-right"><a href=""  class="btn  btn-success  right" type='button'  data-toggle="modal" >  Aceptar </a></div>  --}}
-				 <button class="pull-right" type="submit" class="btn btn-primary">
-                                {{ __('Aceptar') }}
-                            </button>
+				
 		    {{-- <select name="" id="">
 		    	<option value="">seleccionar Vial</option>
 		    </select> --}}
 		   
 		   	<label for=""></label>
                <input   type="number"  name="cantidad" min="0" max="10000" step="1" placeholder="Corte" />
-          
+
+            {{-- <a href="#edit"  class="btn  btn-info  right" type='button'  data-toggle="modal" >Completar datos <i class="glyphicon glyphicon-edit"></i></a> --}}
+
+             <button class="pull-right" type="submit" class="btn btn-primary">
+                                {{ __('Aceptar') }}
+                </button>
         </div>
 
-        <h3 class="page-header">Viales</h3>
+       
+<h3 class="page-header">Datos de los viales</h3>
+        
+        <div class="gallery center col-md-18 col-md-offset-0">
+    <div class="row">
+		
+    	    <h1 class="page-header"></h1>
+            <table class="table table-stripped table-bordered">
+			      <thead>
+			        <tr>
+			         
+			          <th>Naturaleza</th>
+			         
+			          <th>Concentración</th>
+			          <th>Fecha de Entrada</th>
+			          <th>Fecha de Salida</th>
+			          <th>Órgano</th>
+			          <th>Diagnóstico</th>
+			          
+			        </tr>
+			      </thead>
+			      <tbody>
+					
+			        <tr>
+			         
+			          	  <td><input type="text" name="naturaleza" class="form-control"></td>
+				          
+				          <td><input type="text" name="concentracion" class="form-control"></td>
+				          <td><input type="date" name="fecha_entrada" class="form-control"></td>
+				          <td><input type="date" name="fecha_salida" class="form-control"></td>
+				          <td><input type="text" name="organo" class="form-control"></td>
+				          <td><input type="text" name="diagnostico" class="form-control"></td>
+				         
+				         
+			        </tr>
 
+			      
+			      </tbody>
+    		</table>
+			
+	
+	</div>
+</div>
+
+ <h3 class="page-header">Viales</h3>
         <div class="gallery center col-md-18 col-md-offset-0">
     <div class="row">
 		
@@ -71,8 +117,7 @@
 						 <td><button class="editbtn">Ceder</button></td>
 						  <td><a href="#edit{{$vial->id}}"  class="btn  btn-warning  right" type='button'  data-toggle="modal" >   <i class="glyphicon glyphicon-edit"></i></a>
 							  </td>
-   
-				       
+
 			        </tr>	
 					@endforeach
 				
@@ -95,81 +140,9 @@
 </div> --}}
 </div></form>
 						
-	{{--  ventana de editar --}}
-					<div class="modal fade in" id="edit{{$vial->id}}" >
-                      <div class="modal-dialog">
-                        <div class="container">
-                        <div class="row">
-                           <div class="  col-lg-8 col-lg-offset-0  toppad" >
-                              <div class="modal-content">
-                                 <div class="panel panel-default">
-                                   <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span>
-                                    </button>
-                                    <div class="panel-heading">
-                                      <h3 class="panel-title text-center">Editar Corte de Vial {{$vial->muestra_id}} </h3>
-                                    </div>
-                                  </div>
-                                   <div class="card-body">
-					<form method="POST" action="{{url('viales/corte/editar')}}">
-						{{ csrf_field() }}
-                        {{ method_field('PUT') }}
-                         
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-2 col-form-label "> Naturaleza</label>
-
-                            <div class="col-md-4">
-                                <input type="text" name="naturaleza" class="form-control" value="" required autofocus>
-                            </div>
-                        
-                       
-                            <label for="name" class="col-md-2 col-form-label "> Concentración</label>
-
-                            <div class="col-md-4">
-                                <input type="text" name="concentracion" class="form-control" value="" required autofocus>
-                            </div>
-						</div>
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-2 col-form-label "> Fecha de Entrada</label>
-                            <div class="col-md-4">
-                                <input type="date" name="fecha_entrada" class="form-control" value="" required autofocus>                          
-                            </div>
-                       
-                            <label for="name" class="col-md-2 col-form-label "> Fecha de Salida</label>
-                            <div class="col-md-4">
-                                <input type="date" name="fecha_salida" class="form-control" value="" required autofocus>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="name" class="col-md-2 col-form-label "> Órgano</label>
-                            <div class="col-md-4">
-                                <input type="text" name="organo" class="form-control" value="" required autofocus>                          
-                            </div>
-                        
-                            <label for="name" class="col-md-2 col-form-label "> Diagnóstico</label>
-                            <div class="col-md-4">
-                               <input type="text" name="diagnostico" class="form-control" value="" required autofocus>
-                                                     
-                            </div>
-                        </div>
-
-                    
-
-                        <div class="panel-footer">
-                            <button type="submit" class="btn btn-primary">
-                                {{ __('Aceptar') }}
-                            </button>
-                            <span class="pull-right">                        
-                            <a  href="" aria-label="Close" data-original-title="cancelar" data-toggle="tooltip" role ="button" type="button" class="btn  btn-danger  ">Cancelar<i class="glyphicon glyphicon-share-alt"></i></a>
-                            </span>
-                    </div>
-                    
-                </div>
-
-                    </div> </div> </div> </div> </div> </div>
-						</form>
+	
+					
+				
 <script>	
 $(document).ready(function(){
     $('.editbtn').click(function(){
